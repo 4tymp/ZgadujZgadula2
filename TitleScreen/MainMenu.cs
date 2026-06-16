ae452fa28659278c6f3d5e0fee5b6b50f6a8e8cd
tmp <tymoteuszpieczka@gmail.com>
2026-06-16 18:09:25 +0200

    feat: title screen

--- TitleScreen/MainMenu.cs
+++ TitleScreen/MainMenu.cs
@@ -1,12 +1,13 @@
+using ZgadujZgadula2.Utils;
+
 namespace ZgadujZgadula2.TitleScreen;
 
 public class MainMenu
 {
     public static void Show()
     {
-        Console.WriteLine("Witamy w zgadujzgadula!"); //placeholder
-        Console.WriteLine("1. Start Game");
-        Console.WriteLine("2. Options");
-        Console.WriteLine("3. Exit");
+        ASCII.Title();
+
+        Console.WriteLine("1. Nowa gra | 2. Hall of Fame | 3. Ustawienia | 4. Wyjście");
     }
 }
