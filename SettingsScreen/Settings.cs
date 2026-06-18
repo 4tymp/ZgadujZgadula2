692514a903b93e1cfe40e90de759be15750a4065
tmp <tymoteuszpieczka@gmail.com>
2026-06-18 21:40:37 +0200

    edytowano Settings.cs zeby dodac zmiane jezykow

--- SettingsScreen/Settings.cs
+++ SettingsScreen/Settings.cs
@@ -1,9 +1,21 @@
+using ZgadujZgadula2.SettingsScreen.Languages;
+
 namespace ZgadujZgadula2.SettingsScreen;
 
 public class Settings{
     public bool AskForBetMode {get; private set;} = true;
 
+    public Language CurrentLanguage {get; private set;} = new Polish();
+
     public void ToggleAskForBetMode(){
         AskForBetMode = !AskForBetMode; // odwraca askforbetmode.
+    }
+
+    public void ToggleLanguage(){
+        if (CurrentLanguage is Polish){
+            CurrentLanguage = new English();
+        } else{
+            CurrentLanguage = new Polish();
+        }
     }
 }
