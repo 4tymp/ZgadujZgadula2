405a9a861c638347b8e78db95b3ad6cdfbca0e8f
tmp <tymoteuszpieczka@gmail.com>
2026-06-18 16:02:18 +0200

    zaczeto prace nad trybem zakladu - stworzono klase Settings

---
+++ SettingsScreen/Settings.cs
@@ -0,0 +1,5 @@
+namespace ZgadujZgadula2.SettingsScreen;
+
+public class Settings{
+    public bool AskForBetMode {get; private set;} = true;
+}
--- obj/Debug/net10.0/ZgadujZgadula2.AssemblyInfo.cs
+++ obj/Debug/net10.0/ZgadujZgadula2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZgadujZgadula2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+172933b75214ae2d6111e119a662f63d360ce6a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec6509e0acb0b03444e6f1364402a83fa48f7a80")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZgadujZgadula2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZgadujZgadula2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
