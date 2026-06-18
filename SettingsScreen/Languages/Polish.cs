d7a38926b5aeb279eef3cffb1004fc0198ac1f7d
tmp <tymoteuszpieczka@gmail.com>
2026-06-18 21:33:04 +0200

    dalej robione jezyki, dodany polski

---
+++ SettingsScreen/Languages/Polish.cs
@@ -0,0 +1,55 @@
+namespace ZgadujZgadula2.SettingsScreen.Languages;
+
+public class Polish : Language {
+    // DifficultySelection
+    public override string SelectDifficulty => "Wybierz poziom trudności:";
+    public override string DifficultyOptions => "1. Łatwy | 2. Średni | 3. Trudny";
+    public override string InvalidDifficulty => "Niepoprawny wybór. Wybierz poprawną opcję (1-3):";
+    public override string ConfirmDifficulty => "Jesteś pewny, ze chcesz wybrać {0} poziom trudności?\nT/n";
+
+    // MainGameScreen
+    public override string AttemptNumber => "Próba nr: {0}";
+    public override string GuessNumber => "Zgadnij liczbę w przedziale 1 - {0}";
+    public override string MaxAttempts => "Maksymalna ilosc strzałów: {0}";
+
+    // RandomMessages
+    public override List<string> RandomMessages => new List<string> {
+        "Zła odpowiedź, spróbuj jeszcze raz!",
+        "Niestety pudło! Celuj dalej.",
+        "Nie tym razem, pomyśl chwilę!",
+        "Blisko, ale to wciąż nie to.",
+        "Los chciał inaczej. Próbuj dalej!"
+    };
+    public override string TooHigh => "Strzelasz za wysoko.";
+    public override string TooLow => "Strzelasz za nisko.";
+
+    // GameOver
+    public override string Congratulations => "Gratulacje! Zgadłeś liczbę w {0} próbach!";
+    public override string EnterName => "Podaj swoje imię:";
+    public override string ScoreSaved => "Wynik zapisany! Wciśnij Enter aby wrócić do menu.";
+
+    // BadGameOver
+    public override string BetLost => "Niestety, przekroczyłeś limit strzałów ({0}) i przegrałeś zakład.";
+    public override string PressEnter => "Wciśnij Enter, aby wrócić do menu tytułowego.";
+
+    // BetModeSelection
+    public override string BetModeQuestion => "Czy chcesz grać w trybie zakładu?";
+    public override string YesNo => "T/n";
+    public override string EnterMaxAttempts => "Podaj maksymalną liczbę prób:";
+
+    // HallOfFame
+    public override string HallOfFameTitle => "Hall of Fame - {0}";
+    public override string NoScores => "Brak wyników dla tego poziomu trudności.";
+    public override string HallOfFameOptions => "1. Łatwy | 2. Średni | 3. Trudny | 4. Wyjście";
+
+    // Settings
+    public override string SettingsTitle => "Ustawienia";
+    public override string BetModeSetting => "1. Tryb zakładu: {0}";
+    public override string BetModeOn => "Włączony";
+    public override string BetModeOff => "Wyłączony";
+    public override string Exit => "2. Wyjście";
+
+    // MainMenu
+    public override string MainMenuOptions => "1. Nowa gra | 2. Hall of Fame | 3. Ustawienia | 4. Wyjście";
+    public override string SelectOption => "Wybierz poprawną opcję (1-4):";
+}
--- obj/Debug/net10.0/ZgadujZgadula2.AssemblyInfo.cs
+++ obj/Debug/net10.0/ZgadujZgadula2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZgadujZgadula2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8dc25091fe2f9c6294945f28f8345140ae00a8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a26e829d804ce6fa43e81ff0c7a27f7d6cc5a55")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZgadujZgadula2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZgadujZgadula2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
