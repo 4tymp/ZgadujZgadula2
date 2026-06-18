ff81438a65a68f0f834bb66195a6fad7b74a74c8
tmp <tymoteuszpieczka@gmail.com>
2026-06-18 22:55:12 +0200

    aczeto prace nad wyroznieniem ng+ w hall of fame

--- Shared/ScoreEntry.cs
+++ Shared/ScoreEntry.cs
@@ -7,12 +7,14 @@ public class ScoreEntry{
     public Difficulty Difficulty {get;}
     public int Attempts {get;}
     public TimeSpan Time {get;}
+    public bool IsNewGamePlus { get; }
 
-    public ScoreEntry(string playerName, Difficulty difficulty, int attempts, TimeSpan time){
+    public ScoreEntry(string playerName, Difficulty difficulty, int attempts, TimeSpan time, bool isNewGamePlus){
         PlayerName = playerName;
         Difficulty = difficulty;
         Attempts = attempts;
         Time = time;
+        IsNewGamePlus = isNewGamePlus;
     }
 
 }
